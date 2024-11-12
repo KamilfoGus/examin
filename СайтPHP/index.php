c01791b4cb05f9facaa25053aba6ec5a63fcc73c
KamilfoGus <kamikadze203kl@gmail.com>
2024-11-12 18:18:15 +0500

    index.php

--- СайтPHP/index.php
+++ СайтPHP/index.php
@@ -1,3 +1,55 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "nail_appointments";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Установить режим обработки ошибок в PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Установить кодировку UTF-8 для базы данных
+    $conn->exec("SET NAMES utf8mb4");
+} catch(PDOException $e) {
+    echo "Ошибка: " . $e->getMessage();
+}
+    // Обработка регистрации
+if (isset($_POST['register'])) {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    // Хеширование пароля
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Проверка наличия пользователя с таким логином
+    $sql = "SELECT * FROM users WHERE login = :login";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':login', $login);
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+        echo "Пользователь с таким логином уже существует.";
+    } else {
+        // Добавление нового пользователя в базу данных
+        $sql = "INSERT INTO users (name, phone, login, password, email) VALUES (:name, :phone, :login, :password, :email)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':phone', $phone);
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':password', $hashed_password);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        echo "Регистрация прошла успешно!";
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +67,7 @@
     <center><h3>Регистрация</h3></center>
 <form method="POST">
 <div class="Fio">
-    <input type="text" id="FIO" name="FIO" placeholder="ФИО" require>
+    <input type="text" id="name" name="name" placeholder="ФИО" require>
 </div>
 
 <div class="email1">
@@ -25,17 +77,16 @@
     <input type="password" id="password" name="password" placeholder="Пароль"require >
     </div>
     <div class="num">
-    <input type="text" id="number" name="number" placeholder="Номер телефона" require></div>
+    <input type="text" id="phone" name="phone" placeholder="Номер телефона" require></div>
     <div class="log">
     <input type="text" name="login" id="login" placeholder="Логин" require></div>
 
-<div class="but"><button>Зарегистрироваться</button>
+<input class="but1" type="submit" placeholder="Зарегистрироваться">
 <p><a href="auto.php" class="a1"> Войти</a></p>
 <p><a href="admin.php" class="a1">Войти как админ</a></p>
 </div>
 </div>
 </form>
-
 
 </body>
 </html>
